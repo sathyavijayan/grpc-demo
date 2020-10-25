3eb6815e7468b6fc72b8beb159b375359df60bb2
Sathyavijayan Vittal <sathyavijayan@gmail.com>
2020-10-25 15:11:15 +0000

    fix incorrect ns

--- src/translator/service.clj
+++ src/translator/service.clj
@@ -1,4 +1,4 @@
-(ns translation.service
+(ns translator.service
   (:require [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]
             [io.pedestal.http.body-params :as body-params]
